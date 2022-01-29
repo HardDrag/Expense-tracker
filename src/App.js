dcfe14b00c4a6c964cb1fc93a61cb0e8bf2924f7
HardDrag <harddragoffical@gmail.com>
2022-01-29 15:39:24 +0100

    Small changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New version <code>src/App.js</code> testing...
+          Nos
         </p>
         <a
           className="App-link"
